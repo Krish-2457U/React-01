54d1a6ffe851e036e40e026bd9a991e68448ef9e
K Krishna Kumar <krishnakumaracharya3@gmail.com>
2024-08-29 14:57:21 +0530

    Adding npm packages

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "heading" }, [
     React.createElement("div", { id: "child" }, [
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag"),
+        React.createElement("h1", {}, "This is React World"),
+        React.createElement("h2", {}, "By Krishna"),
     ]),
     React.createElement("div", { id: "child" }, [
         React.createElement("h1", {}, "I'm an h1 tag"),
@@ -9,7 +12,7 @@ const parent = React.createElement("div", { id: "heading" }, [
     ]),
 ]);
 
-console.log(parent);
+console.log(parent);//Object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
